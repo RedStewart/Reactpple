b9504747e93272f6a6eb835537d7af15f4640c2b
Reed Stewart <reedostewart@gmail.com>
2019-07-16 14:37:27 +1200

    All grid cards now functioning, still need to add reponsive design

--- src/components/grid/GridCards.js
+++ src/components/grid/GridCards.js
@@ -10,6 +10,7 @@ const GridCards = () => {
         title: 'MacBook Air',
         text: 'Lightness strikes again.',
         imageSrc: 'grid1.png',
+        buttonStyle: 'dark',
         buttons: ['Learn more', 'Buy']
       },
       {
@@ -18,6 +19,7 @@ const GridCards = () => {
         title: 'iPad Pro',
         text: 'All new. All screen. All powerful.',
         imageSrc: 'grid2.png',
+        buttonStyle: 'dark',
         buttons: ['Learn more', 'Buy']
       },
       {
@@ -26,7 +28,36 @@ const GridCards = () => {
         title: 'Meet the closers.',
         text: 'See how Apple Watch owners close their Activity rings.',
         imageSrc: 'grid3.png',
+        buttonStyle: 'light',
         buttons: ['Watch the films']
+      },
+      {
+        id: 4,
+        cardStyle: 'light',
+        title: 'MacBook Pro',
+        text: 'More power. More performance. More pro.',
+        imageSrc: 'grid4.png',
+        buttonStyle: 'dark',
+        buttons: ['Learn more', 'Buy']
+      },
+      {
+        id: 5,
+        cardStyle: 'light',
+        title: 'Music',
+        text: 'Lose yourself in 50 million songs.',
+        imageSrc: 'grid5.png',
+        buttonStyle: 'light',
+        buttons: ['Try it for free*', 'Learn more']
+      },
+      {
+        id: 6,
+        cardStyle: 'dark',
+        title: 'AirPods',
+        text:
+          'Wireless charging. Voice-activated Siri. More magical than ever.',
+        imageSrc: 'grid6.png',
+        buttonStyle: 'light',
+        buttons: ['Learn more', 'Buy']
       }
     ]
   });
--- src/components/grid/GridSingleCard.js
+++ src/components/grid/GridSingleCard.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
 const GridSingleCard = ({
-  card: { cardStyle, title, text, imageSrc, buttons }
+  card: { cardStyle, title, text, imageSrc, buttonStyle, buttons }
 }) => {
-  console.log(buttons);
-
   const bgImage = require(`../../images/${imageSrc}`);
   const cardTheme =
     cardStyle === 'light' ? { color: '#FFFFFF' } : { color: '#111111' };
 
-  let style = {
+  const buttonTheme =
+    buttonStyle === 'dark' ? { color: '#0070C9' } : { color: '#FFFFFF' };
+
+  const style = {
     background: `no-repeat url(${bgImage}) 50% / 100%`,
     backgroundSize: 'unset',
     height: '580px',
@@ -17,17 +18,27 @@ const GridSingleCard = ({
     color: cardTheme.color
   };
 
+  const btnStyle = {
+    color: buttonTheme.color
+  };
+
   return (
     <div style={style}>
       <h1 className=''>{title}</h1>
       <p>{text}</p>
 
-      <button className='btn-card'>
+      {buttons.map((btn, index) => (
+        <button className='btn-card' key={index} style={btnStyle}>
+          <span className='btn-card-text'>{btn}</span> >
+        </button>
+      ))}
+
+      {/* <button className='btn-card'>
         <span className='btn-card-text'>Learn more</span> >
       </button>
       <button className='btn-card btn-grid'>
         <span className='btn-card-text'>Buy</span> >
-      </button>
+      </button> */}
     </div>
   );
 };
